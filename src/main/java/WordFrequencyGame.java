79428547d26ccfff195bd06e823ea5dc43b431a7
hardy <hard.t.z.wei@oocl.com>
2020-07-27 15:52:11 +0800

    [hardy]refactor:rename variable

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -33,7 +33,7 @@ public class WordFrequencyGame {
                 }
                 wordInfos = list;
 
-                wordInfos.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                wordInfos.sort((firstWordInfo, secondWordInfo) -> secondWordInfo.getWordCount() - firstWordInfo.getWordCount());
 
                 StringJoiner joiner = new StringJoiner(DELIMITER);
                 for (WordInfo w : wordInfos) {
